27ebdf89a84cc2378ffce499ffd07c769e52b158
Abhishek Singh <abhisheksngh9415@gmail.com>
2022-06-24 19:10:34 +0530

    merge k sorted linked lists

---
+++ Blind 75/Linked_List/4.Merge_K_Sorted_LinkedList.cpp
@@ -0,0 +1,197 @@
+/*
+	A B H I S H E K    S I N G H
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define all_rev(x) (x).rbegin(), (x).rend()
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+
+/*============================================================*/
+/*    debug() functions */
+
+template <class T> void _print(T t) {cerr << t;}  
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*==============================================================*/
+/*    Newly added templates */
+
+template <class T> void print_vec(vector<T> v);  //printing any vector
+template <class T> void print_set(set<T> s);  //printing set of any datatype
+template <class T> void input_vec(vector<T> &v , int n);  //taking input of n elements of any data type into a vector
+template <class T, class V, class X>V binarySearch(vector<T> a, V n, X item);  //binary search on vector
+template <class T> void print_vec(vector<T> v){ for(T i:v){ cout<<i<<" ";}cout<<nline;} 
+template <class T> void print_set(set<T> s){for(T i:s){cout<<i<<" ";}cout<<nline;}  
+template <class T> void input_vec(vector<T> &v , int n){for(int i=0;i<n;i++){int temp;cin>>temp;v.pb(temp);}}   
+template <class T, class V, class X>V binarySearch(vector<T> a, V n, X item){  V low = 0; V high = n - 1; while(low<=high){{V mid = (low + high)/2; if (a[mid] == item) return mid; else if (a[mid] < item) low = mid + 1; else high = mid - 1;}} return -1;}
+
+/*===============================================================*/
+
+/*  Special puropose functions */
+
+// ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+// ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+// void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+// ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+// ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+// bool revsort(ll a, ll b) {return a > b;}
+// void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+// ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+// void google(int t) {cout << "Case #" << t << ": ";}
+// vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+// ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+// ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+// ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+// ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+// ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+// ll lcm(int a, int b){return (a / gcd(a, b)) * b;}
+
+/*=================================================================*/
+class Node{
+public:
+    int data;
+    Node* next;
+    Node(){
+        next = nullptr;
+    }
+};
+Node* createNode(int data){
+    Node* new_node = new Node();
+    new_node->data = data;
+    return new_node;
+}
+
+void printLinkedList(Node** head){
+    cout<<"Printing..."<<nline;
+    Node* temp = *head;
+    while(temp!=nullptr){
+        cout<<temp->data<<nline;
+        temp= temp->next;
+    }
+}
+
+Node* mergeKLists(vector<Node*>& lists) {
+    Node* head_neww=NULL;
+    for(int i=0;i<lists.size();i++){
+    	Node* tail=NULL;
+    	Node* head_new = NULL;
+    	Node* list1 = lists[i];
+    	Node* list2 = head_neww;
+
+    	if(list1==NULL){
+    		head_new = list2;
+    		head_neww = head_new;
+    		continue;
+    	}
+    	if(list2 == NULL){
+    		head_new = list1;
+    		head_neww = head_new;
+    		continue;
+    	}
+
+	    while(list1!=NULL && list2!=NULL){
+	        if(list1->data < list2->data){
+	            if(head_new ==NULL)
+	                head_new=list1;
+	            else
+	                tail->next=list1;
+	            tail=list1;
+	            list1=list1->next;
+	        }
+	        else{
+	            if(head_new ==NULL)
+	                head_new=list2;
+	            else
+	                tail->next=list2;
+	            tail=list2;
+	            list2=list2->next;
+	        }
+	    }
+	    if(list1!=NULL)
+	        tail->next=list1;
+	    if(list2!=NULL)
+	        tail->next=list2;
+	    head_neww = head_new;
+	}
+    return head_neww;
+}
+
+void solve() {
+	Node* head2 = NULL;
+    head2= createNode(0);
+    Node* temp2 = head2;
+    for(int i=1;i<4;i++){
+        Node* curr_node = createNode(i);
+        temp2->next = curr_node;
+        temp2= curr_node;
+    }
+    Node* head3 = createNode(1);
+    Node* head4 = createNode(2);
+    Node* head5 = createNode(5);
+    head3->next = head4;
+    head4->next = head5;
+    printLinkedList(&head2);
+    printLinkedList(&head3);
+
+    Node* head6 = createNode(1);
+    Node* head7 = createNode(3);
+    Node* head8 = createNode(9);
+    head6->next = head7;
+    head7->next = head8;
+    printLinkedList(&head6);
+    vector<Node*> lists = {head2,head3,head6};
+    Node* new_list = mergeKLists(lists);
+    printLinkedList(&new_list);
+
+}
+
+
+int main() {
+	fastio();
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	solve();
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration . count() / 1000 << endl;
+#endif
+	return 0;
+}
