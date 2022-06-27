1d413d4b19b3c81a38277d70794817e8b5ee492b
Abhishek Singh <abhisheksngh9415@gmail.com>
2022-06-27 18:01:11 +0530

    Leetcode-429 N-Ary tree level order traversal

---
+++ Leetcode/429.N_Ary_Tree_Level_Order_Traversal.cpp
@@ -0,0 +1,21 @@
+vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> ans;
+        if(root==NULL)
+            return ans;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            vector<int> level;
+            for(int i=0;i<n;i++){
+                Node* temp = q.front();
+                q.pop();
+                for(auto it : temp->children)
+                    if(it!=NULL)
+                        q.push(it);
+                level.push_back(temp->val);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
