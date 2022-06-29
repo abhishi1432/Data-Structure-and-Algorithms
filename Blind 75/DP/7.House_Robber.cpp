0cfa107012fcb1317dafed88d55c62d20580d000
Abhishek Singh <abhisheksngh9415@gmail.com>
2022-06-29 17:39:04 +0530

    house robber

---
+++ Blind 75/DP/7.House_Robber.cpp
@@ -0,0 +1,160 @@
+/*
+	A B H I S H E K    S I N G H
+	S.no : 11
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define all_rev(x) (x).rbegin(), (x).rend()
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+
+/*============================================================*/
+/*    debug() functions */
+
+template <class T> void _print(T t) {cerr << t;}  
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*==============================================================*/
+/*    Newly added templates */
+
+template <class T> void print_vec(vector<T> v);  //printing any vector
+template <class T> void print_set(set<T> s);  //printing set of any datatype
+template <class T> void input_vec(vector<T> &v , int n);  //taking input of n elements of any data type into a vector
+template <class T, class V, class X>V binarySearch(vector<T> a, V n, X item);  //binary search on vector
+template <class T> void print_vec(vector<T> v){ for(T i:v){ cout<<i<<" ";}cout<<nline;} 
+template <class T> void print_set(set<T> s){for(T i:s){cout<<i<<" ";}cout<<nline;}  
+template <class T> void input_vec(vector<T> &v , int n){for(int i=0;i<n;i++){int temp;cin>>temp;v.pb(temp);}}   
+template <class T, class V, class X>V binarySearch(vector<T> a, V n, X item){  V low = 0; V high = n - 1; while(low<=high){{V mid = (low + high)/2; if (a[mid] == item) return mid; else if (a[mid] < item) low = mid + 1; else high = mid - 1;}} return -1;}
+
+/*===============================================================*/
+
+/*  Special puropose functions */
+
+// ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+// ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+// void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+// ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+// ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+// bool revsort(ll a, ll b) {return a > b;}
+// void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+// ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+// void google(int t) {cout << "Case #" << t << ": ";}
+// vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+// ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+// ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+// ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+// ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+// ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+// ll lcm(int a, int b){return (a / gcd(a, b)) * b;}
+
+/*=================================================================*/
+int naive_rob_Helper(vector<int>& nums,int ind,int n){
+	/*
+		Time complexity of this solution is O(2^n).
+		Space compleixity is O(1)
+		Approach : We have used the concept of taking and not taking a element.
+		-If you took a element then move to the index+2 as you cant take adjacent elements.
+		-If you did not took the element then move 1 index forward.
+		-Base Case : If we reach at last element ,it means the previous elementn was not taken so we have to take this last element , so return that element.
+
+	*/
+	if(ind==n-1)
+		return nums[ind];
+	if(ind>=n)
+		return 0;
+	int take_current = nums[ind] + naive_rob_Helper(nums,ind+2,n);
+	int leave_current = 0 + naive_rob_Helper(nums,ind+1,n);
+	return max(take_current,leave_current);
+}
+int memo_rob_Helper(vector<int>& nums,int ind,int n,vector<int>&memo){
+	/*
+		Time complexity of this solution is O(2^n).
+		Space compleixity is O(1)
+	*/
+	if(ind==n-1)
+		return nums[ind];
+	if(ind>=n)
+		return 0;
+	int take_current;
+	if(memo[ind+2]!=-1)
+		take_current = nums[ind] + memo[ind+2];
+	else{
+		memo[ind+2] = memo_rob_Helper(nums,ind+2,n,memo); 
+		take_current = nums[ind] + memo[ind+2];
+	}
+	int leave_current;
+	if(memo[ind+1]!=-1)
+		leave_current = 0 + memo[ind+1];
+	else{
+		memo[ind+1] = memo_rob_Helper(nums,ind+1,n,memo);
+		leave_current = 0 + memo[ind+1];
+	}
+	return max(take_current,leave_current);
+}
+
+int rob(vector<int>& nums) {
+	int n=nums.size();
+      return naive_rob_Helper(nums,0,n);
+      vector<int>memo(n+1,-1);
+      return memo_rob_Helper(nums,0,n,memo);
+}
+
+void solve() {
+	int n;
+	cin>>n;
+	vector<int> arr;
+	input_vec(arr,n);
+	cout<<rob(arr)<<nline;	
+
+}
+
+
+int main() {
+	fastio();
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	solve();
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration . count() / 1000 << endl;
+#endif
+	return 0;
+}
