d241c48484b35e620c8eb34ef1c6559fff8c5696
Abhishek Singh <abhisheksngh9415@gmail.com>
2022-07-05 17:20:35 +0530

    100.Same tree

---
+++ LeetCode/100.Same_Tree.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q)
+            return true;
+        else if((!p && q) || (p && !q))
+            return false;
+        if(p->val == q->val)   //if roots , left, right of both tree are equal respectively then AND of these will be true
+            return true & isSameTree(p->left, q->left) & isSameTree(p->right , q->right);
+        else  //if any of the values is not equal, return false, no need to traverse further
+            return false;
+};
